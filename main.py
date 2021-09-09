7108e1a75c58269789b809f5a4d90439cd3b1053
Soare95 <83647957+Soare95@users.noreply.github.com>
2021-09-09 12:19:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdrive
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from selenium.webdriver.common.keys import Keys
 import time
